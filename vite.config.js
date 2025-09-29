8f5bafb4389a7605041eacd91e035193fbc7154e
KaiMaiRuh <zerono1270@gmail.com>
2025-09-30 00:46:08 +0700

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Numerical-web/',   // 👈 เพิ่มบรรทัดนี้
 })
